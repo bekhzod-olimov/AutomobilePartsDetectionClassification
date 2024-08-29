c776d5e341119e8528e96d7af8ff00958c4ff1ef
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2024-08-29 13:26:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,22 @@ def run(args):
     with open(f"saved_dls/{args.data}_cls_names.pkl", "rb") as f: cls_names = pickle.load(f)
     # Get transformations to be applied
     tfs = get_tfs((224, 224))[1]
+    # Set the urls to download pretrained weights for every model
     url = "https://drive.google.com/file/d/1RaUVf1uadEHyhk-lUX4Kuaxgm3CiA4DI/view?usp=sharing" if args.data == "genesis" else ("https://drive.google.com/file/d/1m2xHGDxCG1XzKIYr00M-ugVlTAXuE7Ct/view?usp=share_link" if args.data == "kia" else "https://drive.google.com/file/d/1BUz7QKCfAOXXgtITJnmSisQxuv9txA1d/view?usp=share_link")
+    # Load AI model
     model = load_model(model_name = args.model_name, num_classes = len(cls_names), checkpoint_path = f"ckpts/{args.data}_best.ckpt", url = url)
 
     @app.route('/predict', methods=['POST'])
     def predict():
+
+        """
+        This functon classifies an input image and converts the output into json format.
+
+        Output:
+
+             json    - jsonified prediction of the AI model.
+        
+        """
         im = json.loads(request.data)['im']
         jpg_original = base64.b64decode(im)
 
