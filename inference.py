8c5a304b4d3fcc32a672adce527ce2467e4e3f5b
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2024-10-02 13:40:35 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,8 +1,8 @@
+# Import libraries
 import os, torch, argparse, pickle
 from utils import get_state_dict, get_model, get_fm, create_dbs, predict_classes_new, excel_summary
 from dataset import CustomDataset, InferenceCustomDataset
-from transformations import get_tfs
-from torch.utils.data import DataLoader
+from transformations import get_tfs; from torch.utils.data import DataLoader
 
 def run(args):
     
@@ -25,20 +25,23 @@ def run(args):
         print(f"There are {len(inf_dl)} batches in the test dataloader!")
         print(f"There are {len(cls_names)} classes in the test dataloader!")
     
+    # Set model path
     model_path = args.saved_model_path + args.data_name + "_best_model_" + args.model_name + "_new_classes.ckpt"
     
+    # Load model
     model = get_model(model_name = args.model_name, n_cls = len(cls_names), lang = args.lang,
                       device = args.device, saved_model_path = model_path)
     
     qry_fms_all, im_lbls = create_dbs(model = model, test_dl = dl, model_name = args.model_name, lang = args.lang,
                                                  data_name = args.data_name, device = args.device, save_path = "train_dbs")
     
+    # Clear GPU
     torch.cuda.empty_cache()
     
+    # Predict classes
     predict_classes_new(model = model, qry_fms_all = qry_fms_all, im_lbls = im_lbls, test_dl = inf_dl,
                         model_name = args.model_name, data_name = args.data_name, lang = args.lang,
                         device = args.device, cls_names = cls_names, num_top_stop = 5, top_k = 5, save_path = "inf_dbs")
-        
 
 if __name__ == "__main__":
     
